4388792d9c63631c1609cebef51c04806a9ceaa2
Mark Michaelis <Mark@IntelliTect.com>
2015-03-14 11:04:13 -0700

    Moved Hello Inigo message to HomeController.cs

--- WebApplicationSample/Controllers/HomeController.cs
+++ WebApplicationSample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Mvc;
+using System;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,6 +10,8 @@ namespace WebApplicationSample.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+            ViewBag.HelloWorld = $"{ DateTime.Now }: Hello! My name is Inigo Montoya.";
+
             return View();
         }
     }
