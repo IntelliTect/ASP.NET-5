94b74d1167dc8791bcbdd74b3b3fb5960c765770
Mark Michaelis <Mark@IntelliTect.com>
2015-03-14 09:35:03 -0700

    Added Home Controller and View (in corresponding folders)

--- WebApplicationSample/Startup.cs
+++ WebApplicationSample/Startup.cs
@@ -10,11 +10,12 @@ namespace WebApplicationSample
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseWelcomePage("/.");
+            app.UseMvc(); // Default routing out of the box.
         }
     }
 }
